271ed46574876a8497cd7c7c8172e71f2f135d8d
Nogarremi <56323690+nogarremi@users.noreply.github.com>
2024-01-12 12:15:26 -0800

    2024-01-12: Move boto3 functions (#35)

--- src/command_reg.py
+++ src/command_reg.py
@@ -1,7 +1,25 @@
 from command_list import commands
 from requests import put
 
-from utils import get_secrets
+from boto3 import session as boto_session
+from botocore import ClientError
+
+def get_secrets(keys):
+    session = boto_session.Session()
+    client = session.client(service_name='secretsmanager', region_name="us-west-2")
+
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId="Discord-FGC-RS-Bot")
+    except ClientError as e:
+        raise e
+
+    secretstring = json_loads(get_secret_value_response['SecretString'])
+
+    secrets = {}
+    for key in keys:
+        secrets[key] = secretstring[key]
+
+    return secrets
 
 def reg_commands():
     secrets = get_secrets(['fgc-rs-bot-app-id', 'fgc-rs-bot-server-id', 'fgc-rs-bot-token'])
